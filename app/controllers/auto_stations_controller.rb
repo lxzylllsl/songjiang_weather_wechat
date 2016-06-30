391d40a8fa588821e310123ad72c907a5bcea46e
shtzr840329 <shtzr840329@gmail.com>
2016-06-30 22:21:07 +0800

    fix

--- app/controllers/auto_stations_controller.rb
+++ app/controllers/auto_stations_controller.rb
@@ -6,11 +6,11 @@ class AutoStationsController < ApplicationController
     data = AutoStation::History.new.fetch _type, _sitenumber
     @result = []
     data.each do |e| 
-      @result << {
+      @result.unshift({
         name: e['name'],
         datetime: e['datetime'].split(' ')[-1],
         data: e['data']
-      }
+      })
     end
   end
 end
