7b9a40b76fbf02e96f693b827bdc7b7637916f8d
shtzr840329 <shtzr840329@gmail.com>
2016-07-07 22:48:49 +0800

    fix

--- app/controllers/typhoon_controller.rb
+++ app/controllers/typhoon_controller.rb
@@ -2,6 +2,8 @@ class TyphoonController < ApplicationController
   layout 'typhoon'
 
   def index
+    # 计数器
+    $redis.lpush "read_typhoon", {time: DateTime.now}
     # @typhoon = Typhoon::TyphoonDetail.new.fetch('-')
   end
 
