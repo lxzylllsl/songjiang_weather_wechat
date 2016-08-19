11219eb0db562be5945fd6a8ffcae153d7dc6f99
zhangliugang <www.zhangyi508@gmail.com>
2016-08-19 22:40:51 +0800

    fix rabar

--- app/models/radar.rb
+++ app/models/radar.rb
@@ -32,7 +32,7 @@ class Radar
       tri.fill(red)
       # tri.circle(300, 300, 302,302) #中心点位置
       locate_ptx = 290 + offset_x
-      locate_pty = 300 + offset_y
+      locate_pty = 302 + offset_y
       tri.circle(locate_ptx, locate_pty, locate_ptx + 2, locate_pty + 2)
       tri.draw(image)
       file_name = "radar/#{_item['time']}_#{_lon}_#{_lat}.png"
