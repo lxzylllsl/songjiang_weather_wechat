624d21b1c3236883a5c6aa4e8c9a4b56309d9574
mumumumushu <mumumumushu@gmail.com>
2017-03-12 13:06:40 +0800

    use Regexp to fix the translate bug of rmmseg-cpp

--- app/models/concerns/syncopate.rb
+++ app/models/concerns/syncopate.rb
@@ -5,6 +5,9 @@ module Syncopate
   def analyzed text
     return [] if text.blank?
     results = []
+    RMMSeg::Dictionary.dictionaries += [
+        [:words, "weather_words.dic"]
+      ]
     RMMSeg::Dictionary.load_dictionaries
     algor = RMMSeg::Algorithm.new(text)
     
@@ -12,6 +15,11 @@ module Syncopate
         tok = algor.next_token
         break if tok.nil?
         results << tok.text.force_encoding("UTF-8")
+    end
+
+    if results.count == 1 && results[0].include?('转')
+      match_ary = /(.*)转(.*)/.match(results[0])
+      results = [match_ary[1], match_ary[2]]
     end
     return results
   end
