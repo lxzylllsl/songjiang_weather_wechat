88184431f7c163d322b323fad94253cd8a54be82
ivan Lan <mumumumushu@gmail.com>
2017-07-03 11:52:06 +0800

    Change the redis-url of weixin_authorize

--- config/initializers/weixin_authorize.rb
+++ config/initializers/weixin_authorize.rb
@@ -1,5 +1,5 @@
 namespace = "healthy_weather_wechat:weixin_authorize"
-redis = Redis.new(:host => "127.0.0.1", :port => "6380", :db => 1)
+redis = Redis.new(url: ENV['REDIS_URL'], :db => 1)
 
 # 每次重启时，会把当前的命令空间所有的access_token 清除掉。
 exist_keys = redis.keys("#{namespace}:*")
