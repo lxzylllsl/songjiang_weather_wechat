45a73db3cc9b0ba61b115f259324233ab510ef3b
iboying <weboying@gmail.com>
2017-07-29 19:09:59 +0800

    Add wechat share and change typhoon source

--- config/initializers/weixin_rails_middleware_sample.rb
+++ config/initializers/weixin_rails_middleware_sample.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 # Use this hook to configure WeixinRailsMiddleware bahaviors.
 WeixinRailsMiddleware.configure do |config|
-
   ## NOTE:
   ## If you config all them, it will use `weixin_token_string` default
 
@@ -13,9 +12,9 @@ WeixinRailsMiddleware.configure do |config|
   ## Here configure is for you DON'T WANT TO SAVE your public account into database ##
   # Or the other configure is fit for only one weixin public_account
   # If you config `weixin_token_string`, so it will directly use it
-  # config.weixin_token_string = '50df6aeb795d30221d54d15c'
+  # config.weixin_token_string = 'weixin_token_string'
   # using to weixin server url to validate the token can be trusted.
-  # config.weixin_secret_string = '_W689Ujnje9Sv0Ikh9NSbqHGPwVIIF7r'
+  # config.weixin_secret_string = 'weixin_secret_string'
   # 加密配置，如果需要加密，配置以下参数
   # config.encoding_aes_key = '9973d6649ba6a215bb9945f4cc3999e9dcad9fd6d76'
   # config.app_id = "your app id"
@@ -23,7 +22,6 @@ WeixinRailsMiddleware.configure do |config|
   ## You can custom your adapter to validate your weixin account ##
   # Wiki https://github.com/lanrion/weixin_rails_middleware/wiki/Custom-Adapter
   # config.custom_adapter = "MyCustomAdapter"
-
 end
 
 $client ||= WeixinAuthorize::Client.new(WeixinRailsMiddleware.config.app_id, WeixinRailsMiddleware.config.weixin_secret_string)
