8796c2179b1c4d20cb232458ac27ac838a4dc9ee
iboying <weboying@gmail.com>
2017-07-31 17:06:25 +0800

    add wechat share

--- config/initializers/weixin_rails_middleware_sample.rb
+++ config/initializers/weixin_rails_middleware_sample.rb
@@ -12,12 +12,12 @@ WeixinRailsMiddleware.configure do |config|
   ## Here configure is for you DON'T WANT TO SAVE your public account into database ##
   # Or the other configure is fit for only one weixin public_account
   # If you config `weixin_token_string`, so it will directly use it
-  # config.weixin_token_string = 'weixin_token_string'
+  config.weixin_token_string = 'token_string'
   # using to weixin server url to validate the token can be trusted.
-  # config.weixin_secret_string = 'weixin_secret_string'
+  config.weixin_secret_string = 'secret_string'
   # 加密配置，如果需要加密，配置以下参数
   # config.encoding_aes_key = '9973d6649ba6a215bb9945f4cc3999e9dcad9fd6d76'
-  # config.app_id = "your app id"
+  config.app_id = "app_id"
 
   ## You can custom your adapter to validate your weixin account ##
   # Wiki https://github.com/lanrion/weixin_rails_middleware/wiki/Custom-Adapter
