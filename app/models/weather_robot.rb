d5934e212143f300cca5cd89f02572d6776f318f
ivan Lan <mumumumushu@gmail.com>
2017-08-01 22:41:01 +0800

    Fix WeatherRobot but it still have bug with weixin

--- app/models/weather_robot.rb
+++ app/models/weather_robot.rb
@@ -1,42 +1,38 @@
 class WeatherRobot
-	def self.get_reply question
-		require 'uri'
-		require 'net/http'
-		require 'net/https'
+	class << self
+		def get_reply question
+			require 'uri'
+			require 'net/http'
 
-		uri = URI.parse("http://xiaoi.weather-huayun.com")
-		https = Net::HTTP.new(uri.host,uri.port)
-		# https.use_ssl = true
-		req = Net::HTTP::Post.new(uri.path + "?platform=weixin&question=#{question}")
-		# , initheader = {'X-Auth' => create_x_auth}
-		req['X-Auth'] = WeatherRobot.create_x_auth
-		# req['foo'] = 'bar'
-		# req.body = {
-		#  	"platform" => "weixin"
-		# }
-		res = https.request(req)
-		# puts "Response #{res.code} #{res.message}: #{res.body}"
-		res.body.strip
-	end
-	
-private
+			uri = URI('http://xiaoi.weather-huayun.com/ask.do')
+			req = Net::HTTP::Post.new(uri)
+			req.set_form_data( platform: "weixin", question: question )
+			req['X-Auth'] = create_x_auth
+			res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+				http.request(req)
+			end
 
-	def self.create_x_auth
-		require 'digest/sha1'
-		app_key = 'qxj_DAMy5MrR9t0P'
-		app_secret = 'OpVOWelokGrMkyqAkFzI'
-		realm = "xiaoi.com" 
-		method = "POST"
-		_uri = "/ask.do" 
-		begin
-			nonce = ''
-			chars = ("0".."9").to_a 
-			1.upto(40) { |i| nonce << chars[rand(chars.size-1)] }
+			res.body.strip
 		end
-		ha1 = Digest::SHA1.hexdigest("#{app_key}:#{realm}:#{app_secret}")
-		ha2 = Digest::SHA1.hexdigest("#{method}:#{_uri}")
-		ha3 = Digest::SHA1.hexdigest("#{ha1}:#{nonce}:#{ha2}")
+		
+	private
+		def create_x_auth
+			require 'digest/sha1'
+			app_key = 'qxj_DAMy5MrR9t0P'
+			app_secret = 'OpVOWelokGrMkyqAkFzI'
+			realm = "xiaoi.com" 
+			method = "POST"
+			_uri = "/ask.do" 
+			begin
+				nonce = ''
+				chars = ("0".."9").to_a 
+				1.upto(40) { |i| nonce << chars[rand(chars.size-1)] }
+			end
+			ha1 = Digest::SHA1.hexdigest("#{app_key}:#{realm}:#{app_secret}")
+			ha2 = Digest::SHA1.hexdigest("#{method}:#{_uri}")
+			ha3 = Digest::SHA1.hexdigest("#{ha1}:#{nonce}:#{ha2}")
 
-		_x_auth = "app_key=\"#{app_key}\",nonce=\"#{nonce}\",signature=\"#{ha3}\""
+			_x_auth = "app_key=\"#{app_key}\",nonce=\"#{nonce}\",signature=\"#{ha3}\""
+		end
 	end
 end
